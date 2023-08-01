07466a73e0e8e4da9818b50e6023407d80e6c1c6
BartoszDobija <bartosz.dobija@buddy.works>
2023-08-01 14:30:28 +0200

    Add book adding logic

--- src/main/java/com/example/BookStore.java
+++ src/main/java/com/example/BookStore.java
@@ -4,8 +4,18 @@ import java.util.List;
 
 public class BookStore {
     List<Book> Books;
-    void AddBook(Book book)
+    void AddBook()
     {
+        Book book = new Book();
+        System.out.println("Adding new book");
+        System.out.println("Author name:");
+        book.Author = System.console().readLine();
+        System.out.println("Book title:");
+        book.Title = System.console().readLine();
+        System.out.println("Book genre:");
+        book.Title = System.console().readLine();
+        System.out.println("Release date:");
+        book.ReleaseDate = Integer.parseInt(System.console().readLine());
         Books.add(book);
     }
 
@@ -21,5 +31,10 @@ public class BookStore {
     Book ShowBook(int id)
     {
         return Books.get(id);
+    }
+
+    Book DeleteBook(int id)
+    {
+        return Books.remove(id);
     }
 }
