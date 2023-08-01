afa901c75660278e935ce6ee5c2dfac7575f6aa6
Mateusz JakobscheBDY <mateusz.jakobsche@buddy.works>
2023-08-01 15:13:22 +0200

    feat: enhanced and repeatable user dialog program

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,25 +9,36 @@ package com.example;
 public class App
 {
     public static void main( String[] args ) {
+        BookStore library = new BookStore();
+        LibraryViewModel viewModel;
         Scanner scanner = new Scanner(System.in);
         String[] messages = {
                 "welcome to library service! \nplease select action: \n1: list books in library \n2: add new book",
                 "the library contains:",
                 "enter book data: ",
-                "wrong operation [insert number number corresponding with the action]"
+                "wrong operation [insert number number corresponding with the action]",
+                "do you wish to continue? 1: yes"
         };
-        System.out.println( messages[0] );
-        int action = scanner.nextInt();
-        switch (action){
-            case 1:
-                System.out.println( messages[1] );
-                break;
-            case 2:
-                System.out.println( messages[2] + "title" ); //etc.
-                break;
-            default:
-                System.out.println( messages[3] );
-                break;
+        boolean exit = false;
+        while(!exit){
+            System.out.println( messages[0] );
+            int action = scanner.nextInt();
+            switch (action){
+                case 1:
+                    System.out.println( messages[1] );
+                    viewModel = new LibraryViewModel(library.Books);
+                    viewModel.displayLibrary();
+                    break;
+                case 2:
+                    library.addBook();
+                    break;
+                default:
+                    System.out.println( messages[3] );
+                    break;
+            }
+            System.out.println( messages[4] );
+            int decision = scanner.nextInt();
+            exit = decision != 1;
         }
     }
 }
