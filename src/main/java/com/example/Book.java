cec4f2d4ac62e9a17de758e46de2cac6d968837a
BartoszDobija <bartosz.dobija@buddy.works>
2023-08-01 13:40:34 +0200

    Add book

--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -1,4 +1,11 @@
 package com.example;
 
+import java.util.Date;
+
 public class Book {
+    String Title;
+    String Author;
+    String Genre;
+    int ReleaseDate;
+    String Content = "Lorem Ipsum itd itp tutaj tresc ksiazki";
 }
