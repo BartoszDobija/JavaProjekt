2fc1237534d7bc2cd70e1379a9d2c2385a5d2bc6
BartoszDobija <bartosz.dobija@buddy.works>
2023-08-01 15:12:11 +0200

    Added sample data

--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -7,4 +7,15 @@ public class Book {
     String genre;
     int releaseDate;
     String content = "Lorem Ipsum itd itp tutaj tresc ksiazki";
+    Book(){
+
+    }
+    Book(int id, String title, String author, String genre, int releaseDate)
+    {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+    }
 }
