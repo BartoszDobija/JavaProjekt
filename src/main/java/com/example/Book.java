1f22765b08b8ed10a1d39033f1ea9d55b5b7c654
BartoszDobija <bartosz.dobija@buddy.works>
2023-08-02 10:58:41 +0200

    change visibility of fields and use get set methods

--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -2,17 +2,17 @@ package com.example;
 
 public class Book {
 
-    int id;
+    private int id;
 
-    String title;
+    private String title;
 
-    String author;
+    private String author;
 
-    String genre;
+    private String genre;
 
-    int releaseDate;
+    private int releaseDate;
 
-    String content = "Lorem Ipsum itd itp tutaj tresc ksiazki";
+    private String content = "Lorem Ipsum itd itp tutaj tresc ksiazki";
 
     Book() {
 
@@ -24,5 +24,53 @@ public class Book {
         this.author = author;
         this.genre = genre;
         this.releaseDate = releaseDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
