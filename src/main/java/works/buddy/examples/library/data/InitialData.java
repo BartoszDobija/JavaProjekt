5374fb4bf1d23c1dd27bb02f47eb723aeba1af20
BartoszDobija <bartosz.dobija@buddy.works>
2023-08-03 08:53:56 +0200

    fixed InitialData

--- src/main/java/works/buddy/examples/library/data/InitialData.java
+++ src/main/java/works/buddy/examples/library/data/InitialData.java
@@ -2,15 +2,16 @@ package works.buddy.examples.library.data;
 
 import works.buddy.examples.library.model.Book;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class InitialData {
 
     public Collection<Book> getSampleBooks() {
-        // TODO: zmiana List.of na coś co nie jest immutable
-        return List.of(new Book(1, "Anna Karenina", "Leo Tolstoy", "Fiction", 1877),
-                new Book(2, "To Kill a Mockingbird", "Harper Lee,", "Coming-of-age story", 1960),
-                new Book(3, "The Great Gatsby", "F. Scott Fitzgerald", "Fiction", 1925));
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(0, "Anna Karenina", "Leo Tolstoy", "Fiction", 1877));
+        books.add(new Book(1, "To Kill a Mockingbird", "Harper Lee,", "Coming-of-age story", 1960));
+        books.add(new Book(2, "The Great Gatsby", "F. Scott Fitzgerald", "Fiction", 1925));
+        return books;
     }
 }
