bc29bf794503c33308d00540718ca0920f6c587c
Mateusz JakobscheBDY <mateusz.jakobsche@buddy.works>
2023-08-08 14:40:04 +0200

    feat: jdbcBookDao available

--- src/main/java/works/buddy/library/main/Main.java
+++ src/main/java/works/buddy/library/main/Main.java
@@ -3,24 +3,15 @@ package works.buddy.library.main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import works.buddy.library.config.AppConfig;
+import works.buddy.library.dao.JdbcBookDAO;
 import works.buddy.library.services.LibraryApp;
 import works.buddy.library.services.Messages;
-
-import java.sql.*;
 
 public class Main {
 
     private static final String MESSAGES_PROPERTIES = "messages.properties";
 
     public static void main(String[] args) {
-
-        //create connection for a server installed in localhost, with a user "root" with no password
-        try{
-            Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/", "root", null);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
         Messages.init(MESSAGES_PROPERTIES);
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         LibraryApp libraryApp = context.getBean(LibraryApp.class);
