9d7d7d6fce43b88e2151ba0ef14bf65e6d5d124a
Mateusz JakobscheBDY <mateusz.jakobsche@buddy.works>
2023-08-08 14:23:49 +0200

    chore: working connection with mariaDB

--- src/main/java/works/buddy/library/main/Main.java
+++ src/main/java/works/buddy/library/main/Main.java
@@ -6,11 +6,21 @@ import works.buddy.library.config.AppConfig;
 import works.buddy.library.services.LibraryApp;
 import works.buddy.library.services.Messages;
 
+import java.sql.*;
+
 public class Main {
 
     private static final String MESSAGES_PROPERTIES = "messages.properties";
 
     public static void main(String[] args) {
+
+        //create connection for a server installed in localhost, with a user "root" with no password
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/", "root", null);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         Messages.init(MESSAGES_PROPERTIES);
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         LibraryApp libraryApp = context.getBean(LibraryApp.class);
