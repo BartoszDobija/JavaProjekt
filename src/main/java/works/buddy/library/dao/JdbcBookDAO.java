ed983de9759ad688ee1d3abbaba754ad15fb5b29
Mateusz JakobscheBDY <mateusz.jakobsche@buddy.works>
2023-08-08 15:45:36 +0200

    feat: displaying library

--- src/main/java/works/buddy/library/dao/JdbcBookDAO.java
+++ src/main/java/works/buddy/library/dao/JdbcBookDAO.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Repository;
 import works.buddy.library.model.Book;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Collection;
 @Repository
 public class JdbcBookDAO implements BookDAO {
     private final Connection connection;
-
+    private static final String DELETE = "DELETE FROM books WHERE id=?";
+    private static final String FIND_ALL = "SELECT * FROM books ORDER BY id";
+    private static final String FIND_BY_ID = "SELECT * FROM books WHERE id=?";
+    private static final String INSERT = "INSERT INTO books (title, author, genre, releaseYear) VALUES(?, ?, ?)";
+    private static final String UPDATE = "UPDATE books SET title=?, author=?, genre=?, releaseYear=? WHERE id=?";
     public JdbcBookDAO() {
         try {
             this.connection = DriverManager.getConnection("jdbc:mariadb://localhost/library", "library", "library");
@@ -30,7 +35,24 @@ public class JdbcBookDAO implements BookDAO {
 
     @Override
     public Collection<Book> findAll() {
-        return null;
+        Collection<Book> books = new ArrayList<>();
+        try{
+            ResultSet resultSet = execute(FIND_ALL);
+            while (resultSet.next()) {
+                Book book = new Book();
+                book.setId(resultSet.getInt("id"));
+                book.setTitle(resultSet.getString("title"));
+                book.setAuthor(resultSet.getString("author"));
+                book.setGenre(resultSet.getString("genre"));
+                book.setReleaseYear(resultSet.getInt("releaseYear"));
+
+                books.add(book);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return books;
     }
 
     @Override
