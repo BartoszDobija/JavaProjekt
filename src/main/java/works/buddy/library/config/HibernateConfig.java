6feadb8822230f0619426e2731f5a80a0bb8c3cc
BartoszDobija <bartosz.dobija@buddy.works>
2023-08-09 10:00:51 +0200

    feat: HibernateBookDAO + problem with PersistenceUnit

--- src/main/java/works/buddy/library/config/HibernateConfig.java
+++ src/main/java/works/buddy/library/config/HibernateConfig.java
@@ -1,16 +1,33 @@
 package works.buddy.library.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.Persistence;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Configuration
 @ComponentScan("works.buddy.library")
 public class HibernateConfig {
 
-    @PersistenceUnit
-    private EntityManagerFactory entityManagerFactory;
+    @Bean
+    public EntityManager entityManager() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("");
+        return entityManagerFactory.createEntityManager();
+    }
+
+    @Bean
+    public Connection connection() {
+        try {
+            return DriverManager.getConnection("jdbc:mariadb://localhost/library", "library", "library");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
--- src/main/java/works/buddy/library/dao/HibernateBookDAO.java
+++ src/main/java/works/buddy/library/dao/HibernateBookDAO.java
@@ -1,12 +1,13 @@
 package works.buddy.library.dao;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 import works.buddy.library.model.Book;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.sql.Connection;
 import java.util.Collection;
 
 @Repository
@@ -14,38 +15,45 @@ import java.util.Collection;
 public class HibernateBookDAO implements BookDAO {
 
     @Autowired
-    private final SessionFactory sessionFactory;
+    @PersistenceContext
+    private final EntityManager entityManager;
 
-    public HibernateBookDAO(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+    @Autowired
+    private final Connection connection;
+
+    public HibernateBookDAO(EntityManager entityManager, Connection connection) {
+        this.entityManager = entityManager;
+        this.connection = connection;
     }
 
     @Override
     public Collection<Book> findAll() {
-        return getCurrentSession().createQuery("from books").list();
+        return entityManager.createQuery("select * from books").getResultList();
     }
 
     @Override
     public void save(Book book) {
-        getCurrentSession().save(book);
-    }
-
-    private Session getCurrentSession() {
-        return sessionFactory.getCurrentSession();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(book);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Book find(Integer id) throws NotFoundException {
-        return getCurrentSession().get(Book.class, id);
+        return entityManager.find(Book.class, id);
     }
 
     @Override
     public void delete(Integer id) throws NotFoundException {
-        getCurrentSession().delete(find(id));
+        entityManager.remove(find(id));
     }
 
     @Override
     public void update(Book book) throws NotFoundException {
-        getCurrentSession().merge(book);
+        entityManager.merge(book);
     }
 }
