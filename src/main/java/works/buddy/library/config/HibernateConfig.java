0b8371fd0160a168050283575e398782b5b7792d
BartoszDobija <bartosz.dobija@buddy.works>
2023-08-10 15:10:11 +0200

    fix: changed PropertySource

--- src/main/java/works/buddy/library/config/HibernateConfig.java
+++ src/main/java/works/buddy/library/config/HibernateConfig.java
@@ -14,7 +14,7 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@PropertySource("dockerMariaDB.properties")
+@PropertySource("hibernate.properties")
 @EnableTransactionManagement
 public class HibernateConfig {
 
