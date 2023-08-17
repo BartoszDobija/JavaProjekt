a8212fda3b8cab25b724ccddc5607b766fd66cf9
BartoszDobija <bartosz.dobija@buddy.works>
2023-08-17 13:05:34 +0200

    feat: run liquibase as spring bean

--- src/main/java/works/buddy/library/config/AppConfig.java
+++ src/main/java/works/buddy/library/config/AppConfig.java
@@ -1,16 +1,30 @@
 package works.buddy.library.config;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import javax.sql.DataSource;
 import java.util.Scanner;
 
 @Configuration
 @ComponentScan("works.buddy.library")
 @Import({HibernateConfig.class, DefaultDataSource.class})
 public class AppConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:dbm/changelog.xml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
 
     @Bean
     public Scanner scanner() {
